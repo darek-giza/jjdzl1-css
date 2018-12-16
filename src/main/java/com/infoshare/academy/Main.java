f2ea177b7630607069886a4094e16f1974c384d0
Kamil Wawer <kamil.wawer@pollub.edu.pl>
2018-12-16 18:48:49 +0100

    Rename carsControl title

--- src/main/java/com/infoshare/academy/Main.java
+++ src/main/java/com/infoshare/academy/Main.java
@@ -1,9 +1,5 @@
 package com.infoshare.academy;
 
-import com.infoshare.academy.model.CarsControl.PrintDescription;
-import com.infoshare.academy.model.cars.Car;
-import com.infoshare.academy.model.cars.CarPassenger;
-
 public class Main {
 
     public static void main(String[] args) {
