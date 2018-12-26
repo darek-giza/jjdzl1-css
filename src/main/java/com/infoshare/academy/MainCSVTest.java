bd978b4aebddf8306c1a19bf1a74595f2c34ea31
arexe <arexepro@gmail.com>
2018-12-26 13:36:55 +0100

    JZL1CSS-35 - refactor of CarMaker(old CarReader) code to work with all car types, add examples

--- src/main/java/com/infoshare/academy/MainCSVTest.java
+++ src/main/java/com/infoshare/academy/MainCSVTest.java
@@ -1,21 +1,17 @@
 package com.infoshare.academy;
 
-import com.infoshare.academy.iostream.FileIO;
-import com.infoshare.academy.iostream.FilePath;
-import com.infoshare.academy.model.cars.Car;
-import com.infoshare.academy.model.cars.CarReader;
-
-import java.nio.file.Paths;
+import com.infoshare.academy.model.carsservice.CarMaker;
 
 public class MainCSVTest {
     public static void main(String[] args) {
-        //Car Objects example
-/*        CarReader.readCar();
-        CarReader.readCar(1);
-        CarReader.readCar(7);
-        System.out.println(FileIO.readFile(FilePath.getCarData()));
-        System.out.println(FileIO.readLine(FilePath.getCarData(),1));*/
-        CarReader.readCarPassenger();
+
+        //Example usage of CarMaker Class:
+        System.out.println("*Pokaż samochód z klasy Car o id=5");
+        CarMaker.readCar(5);
+        System.out.println("*Pokaż samochód z klasy CarPassenger o id=108");
+        CarMaker.readCarPassenger(108);
+        System.out.println("*Pokaż listę samochodów z klasy Car");
+        CarMaker.readCar();
 
     }
 }
