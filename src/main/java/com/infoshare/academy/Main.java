4deadc9e8cb7f618c868d95a662f52b1731c5da8
darek-giza <darek_giza@op.pl>
2018-12-28 14:07:06 +0100

    Remove unneeded coments

--- src/main/java/com/infoshare/academy/Main.java
+++ src/main/java/com/infoshare/academy/Main.java
@@ -6,25 +6,5 @@ public class Main {
 
     public static void main(String[] args) {
         Menu.getUserDataInput();
-
-
-
-       /*
-
-
-        ReservationControl.addReservation();
-
-        try {
-            Date startDate = ReservationControl.getDateFormatter().parse("2018-10-10");
-            Date endDate = ReservationControl.getDateFormatter().parse("2018-10-30");
-
-            System.out.println(ReservationControl.checkIfCarAvailable(1, startDate, endDate));
-        } catch (ParseException e) {
-            System.out.println("Invalid Date");
-        }
-
-
-
-        */
     }
 }
