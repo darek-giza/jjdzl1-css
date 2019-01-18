410bae3f774e62c951082b0b80159707cc6ac13e
arexe <arexepro@gmail.com>
2019-01-18 21:41:58 +0100

    Update CarDescprition to use enums

--- src/main/java/com/infoshare/academy/model/carsservice/CarDescription.java
+++ src/main/java/com/infoshare/academy/model/carsservice/CarDescription.java
@@ -9,25 +9,25 @@ public class CarDescription {
 
     //Show full description for chosen car type (returns String)
     public static String carDescription(Car car) {
-        String description = String.format("Samochód nr %s: %s %s, rok produkcji: %s, źródło paliwa: %s, moc: %s, przebieg: %s", car.getId(), car.getMake(), car.getModel(), car.getYear(), car.getFuelSourceEnum(), car.getEnginePower(), car.getMileage());
+        String description = String.format("Samochód o id=%s: %s %s, rok produkcji: %s, źródło paliwa: %s, moc: %sKM, przebieg: %skm", car.getId(), car.getMake(), car.getModel(), car.getYear(), car.getFuelSourceEnum(), car.getEnginePower(), car.getMileage());
         return description;
     }
 
     public static String carPassengerDescription(CarPassenger carPassenger) {
-        String descriptionPassenger = String.format("Samochód osobowy nr %s: %s %s, " +
+        String descriptionPassenger = String.format("Samochód osobowy o id=%s: %s %s, " +
                 "rok produkcji: %s, źródło paliwa: %s, moc: %s, przebieg: %s," +
-                " typ: %s, kolor: %s, ilość miejsc: %s, skrzynia biegów; %s", carPassenger.getId(), carPassenger.getMake(), carPassenger.getModel(), carPassenger.getYear(), carPassenger.getFuelSourceEnum(), carPassenger.getEnginePower(), carPassenger.getMileage(), carPassenger.getBodyTypeEnum(), carPassenger.getColorEnum(), carPassenger.getSeats(), carPassenger.getTransmissionEnum());
+                " typ: %s, kolor: %s, ilość miejsc: %s, skrzynia biegów; %s", carPassenger.getId(), carPassenger.getMake(), carPassenger.getModel(), carPassenger.getYear(), carPassenger.getFuelSourceEnum().getFuel(), carPassenger.getEnginePower(), carPassenger.getMileage(), carPassenger.getBodyTypeEnum().getMessage(), carPassenger.getColorEnum().getColor(), carPassenger.getSeats(), carPassenger.getTransmissionEnum().getTransmission());
         return descriptionPassenger;
     }
 
     public static String carOffRoadDescription(CarOffRoad carOffRoad) {
-        String descriptionOffRoad = String.format("Samochód terenowy nr %s: %s %s, rok produkcji: %s, źródło paliwa: %s, moc: %s, przebieg: %s, liczba miejsc: %s, napęd na koła: %s, wciągarka: %s, bagażnik: %s ", carOffRoad.getId(), carOffRoad.getMake(), carOffRoad.getModel(), carOffRoad.getYear(), carOffRoad.getFuelSourceEnum(), carOffRoad.getEnginePower(), carOffRoad.getMileage(), carOffRoad.getSeats(), carOffRoad.getDrive(), carOffRoad.getHasWinch(), carOffRoad.getHasTrunk());
+        String descriptionOffRoad = String.format("Samochód terenowy o id=%s: %s %s, rok produkcji: %s, źródło paliwa: %s, moc: %sKM, przebieg: %skm, liczba miejsc: %s, napęd na koła: %s, wciągarka: %s, bagażnik: %s ", carOffRoad.getId(), carOffRoad.getMake(), carOffRoad.getModel(), carOffRoad.getYear(), carOffRoad.getFuelSourceEnum().getFuel(), carOffRoad.getEnginePower(), carOffRoad.getMileage(), carOffRoad.getSeats(), carOffRoad.getDrive(), carOffRoad.getHasWinch(), carOffRoad.getHasTrunk());
         return descriptionOffRoad;
     }
 
     public static String carTruckDescription(CarTruck carTruck) {
-        String descriptionTruck = String.format("Samochód ciężarowy nr %s: %s %s, rok produkcji: %s, źródło paliwa: %s, moc: %s,przebieg: %s, ładowność: %s, przestrzeń ładunkowa: %s, winda: %s "
-                , carTruck.getId(), carTruck.getMake(), carTruck.getModel(), carTruck.getYear(), carTruck.getFuelSourceEnum(), carTruck.getEnginePower(), carTruck.getMileage(), carTruck.getCapacity(), carTruck.getCargoSpace(), carTruck.getHasLift());
+        String descriptionTruck = String.format("Samochód ciężarowy o id=%s: %s %s, rok produkcji: %s, źródło paliwa: %s, moc: %sKM, przebieg: %skm, ładowność: %skg, przestrzeń ładunkowa: %sm3, winda: %s "
+                , carTruck.getId(), carTruck.getMake(), carTruck.getModel(), carTruck.getYear(), carTruck.getFuelSourceEnum().getFuel(), carTruck.getEnginePower(), carTruck.getMileage(), carTruck.getCapacity(), carTruck.getCargoSpace(), carTruck.getHasLift());
         return descriptionTruck;
     }
 
