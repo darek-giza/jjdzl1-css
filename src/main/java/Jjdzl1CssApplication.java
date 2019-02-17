15484befca5042e75259d2d9ce021e4622736dc8
arexe <arexepro@gmail.com>
2019-02-17 14:10:20 +0100

    Add Spring dependency

---
+++ src/main/java/Jjdzl1CssApplication.java
@@ -0,0 +1,12 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jjdzl1CssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jjdzl1CssApplication.class, args);
+	}
+
+}
+
